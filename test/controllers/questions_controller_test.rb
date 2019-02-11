2baad627e129fdf983f46fc3ad0ba261d0d4a694
Eleonore Breton <eleonore.breton@hec.edu>
2019-02-11 14:36:59 +0100

    avec optionnel

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get ask" do
-    get questions_ask_url
-    assert_response :success
+  def questions_ask_url
+    test "should get ask" do
+      get questions_ask_url
+      assert_response :success
+    end
   end
 
-  test "should get answer" do
-    get questions_answer_url
-    assert_response :success
+  def questions_answer_url
+    test "should get answer" do
+      get questions_answer_url
+      assert_response :success
+    end
   end
 
 end
